cf03a1219d9bf575a4d915a36bf9982cb42c397c
Gabriel Riegner <gabriel.riegner@gmail.com>
2025-02-11 14:11:12 -0800

    add test conditions

--- drift_diffusion/tests/test_model.py
+++ drift_diffusion/tests/test_model.py
@@ -5,9 +5,22 @@ from drift_diffusion.sim import sample_from_pdf
 
 
 def test_drift_diffusion_model():
+
     a, v, z = 1, 0.25, 0
+
     X, y = sample_from_pdf(a, v, z, random_state=0)
+
     ddm = DriftDiffusionModel(z=z, cov_estimator="all")
     ddm.fit(X, y)
     params_ = ddm.params_
+    covariance_ = ddm.covariance_.values()
+
+    # test parameters close to true values
     testing.assert_allclose([a, v], params_, atol=0.05)
+
+    # test covariance matrix of expected shape
+    for cov in covariance_:
+        assert cov.shape == (2, 2)
+
+    # test cov_estimator="all" returns four matrices
+    assert len(covariance_) == 4
