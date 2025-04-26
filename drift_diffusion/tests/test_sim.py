e624a0bc9e90ec777356832cb5ccf5cd5c038386
Gabriel Riegner <gabriel.riegner@gmail.com>
2025-04-25 17:22:15 -0700

    test variability parameters introduce variance

--- drift_diffusion/tests/test_sim.py
+++ drift_diffusion/tests/test_sim.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+from numpy import testing
 
 from drift_diffusion.sim import sim_ddm
 
@@ -9,6 +10,12 @@ def test_sim_ddm():
     x = sim_ddm(dt=0.01, seed=0)
     assert isinstance(x, np.ndarray)
     assert len(x) > 100
+
+    # test that st, sz, sv introduce variability
+    x1 = sim_ddm(dt=0.01, st=0.1, sz=0.1, sv=0.1, seed=0)
+    x2 = sim_ddm(dt=0.01, st=1, sz=1, sv=1, seed=0)
+    with pytest.raises(AssertionError):
+        testing.assert_array_equal(x1, x2)
 
 
 def test_sim_ddm_raises():
