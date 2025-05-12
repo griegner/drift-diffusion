fef82ddd8660e4103a146f9b3a73fecdb01d6cc8
Gabriel Riegner <gabriel.riegner@gmail.com>
2025-05-12 13:03:09 -0700

    fit mixture to empirical and simulated rt + acc

--- examples/utils.py
+++ examples/utils.py
@@ -7,6 +7,7 @@ from scipy.io import loadmat
 from statsmodels.tsa.stattools import acf, pacf
 
 from drift_diffusion.model import DriftDiffusionModel
+from drift_diffusion.model.pdf import mdf, pdf
 
 plt.rcParams.update({"font.size": 9})
 
@@ -74,11 +75,11 @@ def fit_ddm(df, groupby_col):
     return df.groupby(groupby_col)[["coherence", "y"]].apply(_fit_ddm)
 
 
-def plot_fits(df_heatmap, fit_df, x, y, config):
+def plot_fits(df_heatmap, df_fit, x, y, config):
     """
-    plot for heatmap and two estimated variables (e.g., DDM params or RT/ACC).
+    plots heatmap and parameter estimates (e.g., DDM params or RT/ACC).
     `config` is a dict with keys:
-        - var1, var2: column names in fit_df
+        - var1, var2: column names in df_fit
         - var1_lim, var2_lim: axis limits
         - var1_label, var2_label: axis labels
         - corr_label, acf_label1, acf_label2: label templates (use {} for variable names)
@@ -125,38 +126,38 @@ def plot_fits(df_heatmap, fit_df, x, y, config):
     cbar.ax.tick_params(labelsize=9)
 
     # (b) scatterplot
-    corr_ = fit_df[config["var1"]].corr(fit_df[config["var2"]])
+    corr_ = df_fit[config["var1"]].corr(df_fit[config["var2"]])
     label = config["corr_label"].format(corr=corr_)
-    axs["b"].scatter(fit_df[config["var1"]], fit_df[config["var2"]], s=1, color="k")
+    axs["b"].scatter(df_fit[config["var1"]], df_fit[config["var2"]], s=1, color="k")
     axs["b"].set_title(config["var1_label"], fontsize=9)
     axs["b"].set_ylabel(config["var2_label"])
     axs["b"].text(0.98, 0.02, label, ha="right", va="bottom", transform=axs["b"].transAxes, **text_kwargs)
 
     # (c) var1 by day
-    axs["c"].vlines(fit_df.index, fit_df[f"{config['var1']}-"], fit_df[f"{config['var1']}+"], **vline_kwargs)
+    axs["c"].vlines(df_fit.index, df_fit[f"{config['var1']}-"], df_fit[f"{config['var1']}+"], **vline_kwargs)
     axs["c"].set_ylabel(config["var1_label"])
 
     # (d) var1 hist
     label = (
-        rf"$\bar{{{config['var1']}}}$={fit_df[config['var1']].mean():.2f}"
+        rf"$\bar{{{config['var1']}}}$={df_fit[config['var1']].mean():.2f}"
         "\n  "
-        rf"$\pm${fit_df[config['var1']].std():.2f}"
+        rf"$\pm${df_fit[config['var1']].std():.2f}"
     )
-    axs["d"].hist(fit_df[config["var1"]], **hist_kwargs)
+    axs["d"].hist(df_fit[config["var1"]], **hist_kwargs)
     axs["d"].text(0.1, 0.95, label, va="top", transform=axs["d"].transAxes, **text_kwargs)
     axs["d"].axis("off")
 
     # (e) var1 acf
-    n_lags = len(fit_df) // 3
-    acf_ = acf(fit_df[config["var1"]], nlags=n_lags, fft=True, bartlett_confint=False, missing="conservative")
-    pacf_ = pacf(fit_df[config["var1"]].dropna(), nlags=n_lags)
+    n_lags = len(df_fit) // 3
+    acf_ = acf(df_fit[config["var1"]], nlags=n_lags, fft=True, bartlett_confint=False, missing="conservative")
+    pacf_ = pacf(df_fit[config["var1"]].dropna(), nlags=n_lags)
     axs["e"].plot(acf_, c="k", lw=1, label="acf")
     axs["e"].plot(pacf_, c="gray", lw=1, label="pacf")
     axs["e"].legend()
     axs["e"].set_ylabel(config["acf_label1"])
 
     # (f) var2 by day
-    axs["f"].vlines(fit_df.index, fit_df[f"{config['var2']}-"], fit_df[f"{config['var2']}+"], **vline_kwargs)
+    axs["f"].vlines(df_fit.index, df_fit[f"{config['var2']}-"], df_fit[f"{config['var2']}+"], **vline_kwargs)
     axs["f"].set_ylabel(config["var2_label"])
     axs["f"].set_xlabel(x)
     if x == "hour":
@@ -164,18 +165,62 @@ def plot_fits(df_heatmap, fit_df, x, y, config):
 
     # (g) var2 hist
     label = (
-        rf"$\bar{{{config['var2']}}}$={fit_df[config['var2']].mean():.2f}"
+        rf"$\bar{{{config['var2']}}}$={df_fit[config['var2']].mean():.2f}"
         "\n   "
-        rf"$\pm${fit_df[config['var2']].std():.2f}"
+        rf"$\pm${df_fit[config['var2']].std():.2f}"
     )
-    axs["g"].hist(fit_df[config["var2"]], **hist_kwargs)
+    axs["g"].hist(df_fit[config["var2"]], **hist_kwargs)
     axs["g"].text(0.1, 0.95, label, va="top", transform=axs["g"].transAxes, **text_kwargs)
     axs["g"].axis("off")
 
     # (h) var2 acf
-    acf_ = acf(fit_df[config["var2"]], nlags=n_lags, fft=True, bartlett_confint=False, missing="conservative")
-    pacf_ = pacf(fit_df[config["var2"]].dropna(), nlags=n_lags)
+    acf_ = acf(df_fit[config["var2"]], nlags=n_lags, fft=True, bartlett_confint=False, missing="conservative")
+    pacf_ = pacf(df_fit[config["var2"]].dropna(), nlags=n_lags)
     axs["h"].plot(acf_, c="k", lw=1)
     axs["h"].plot(pacf_, c="gray", lw=1)
     axs["h"].set_ylabel(config["acf_label2"])
     axs["h"].set_xlabel("lag (days)")
+
+
+def plot_mixtures(df, df_fit_by_day, coherence):
+    """plots mixture density distributions and accuracy curves"""
+
+    ddm = DriftDiffusionModel(a="+1", t0=0, v="-1 + coherence", z=0, cov_estimator="sample-hessian")
+    n = 1000
+    y = np.linspace(-4, +4, n)
+
+    fig, axs = plt.subplots(ncols=2, figsize=(8, 2), layout="constrained", width_ratios=[3, 2])
+
+    # empirical
+    axs[0].hist(df["y"], bins=90, density=True, color="k", alpha=0.15, label=r"$\hat{f}_H$")
+    acc = (
+        df["y"]
+        .groupby(pd.qcut(np.abs(df["y"]), q=50), observed=True)
+        .apply(lambda x: (x > 0).astype(int).mean())
+        .rename(index=lambda interval: interval.mid)
+    )
+    axs[1].scatter(acc.index, acc, c="k", alpha=0.25, s=5)
+
+    # a, v mixture
+    weights = df.groupby("day")["y"].count().to_numpy() / len(df)
+    a_s = df_fit_by_day["a"].to_numpy()
+    v_s = (df_fit_by_day["beta_v"] * coherence).to_numpy()
+    f_mix = mdf(y, a=a_s, t0=0, v=v_s, z=0, weights=weights)
+    acc = f_mix[n // 2 :] / (f_mix[n // 2 :] + f_mix[: n // 2][::-1])
+    axs[0].plot(y, f_mix, color="k", alpha=0.75, label=r"$\hat{f}_\text{MIX}$")
+    axs[1].plot(y[n // 2 :], acc, color="k", alpha=0.75)
+
+    # non-mixture
+    a, beta_v = ddm.fit(df, df["y"]).params_
+    v = beta_v * coherence
+    f = pdf(y, a=a, t0=0, v=v, z=0)
+    axs[0].plot(y, f, color="k", ls="--", alpha=0.75, label=r"$\hat{f}_\text{DDM}$")
+    acc = f[n // 2 :] / (f[n // 2 :] + f[: n // 2][::-1])
+    axs[1].plot(y[500:], acc, color="k", ls="--", alpha=0.75)
+
+    # adjust plots
+    axs[0].set_title(f"coherence: {coherence}")
+    axs[0].set_xlim([-4, 4])
+    axs[0].legend()
+    axs[1].set_ylim([0.5, 1])
+    axs[1].set_xlim([0, 4])
